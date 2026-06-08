ae7a9f7adfee06382710717fba50a8eb326e3d03
Ildar Kamalov <ik@adguard.com>
2026-06-08 10:17:37 +0300

    AGDNS-4057 langs

--- client_v2/src/helpers/twosky.ts
+++ client_v2/src/helpers/twosky.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line import/no-relative-packages
 import twosky from 'Twosky';
 
-export const LANGUAGES = twosky[0].languages;
-export const BASE_LOCALE = twosky[0].base_locale;
+const homeV2 = twosky.find((p) => p.project_id === 'home_v2');
+
+export const LANGUAGES = homeV2?.languages ?? {};
+export const BASE_LOCALE = homeV2?.base_locale ?? 'en';
