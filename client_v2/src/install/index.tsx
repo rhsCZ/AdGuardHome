6060b4001fcfbbde0a167548ad8548897583ce19
Ildar Kamalov <ik@adguard.com>
2026-06-08 12:45:09 +0300

    fix setup

--- client_v2/src/install/index.tsx
+++ client_v2/src/install/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
 
 import '../index.pcss';
 import configureStore from '../configureStore';
@@ -15,7 +16,9 @@ const store = configureStore<InstallState>(reducers, {});
 const root = createRoot(document.getElementById('root')!);
 root.render(
     <Provider store={store}>
-        <Setup />
-        <Icons />
+        <HashRouter>
+            <Setup />
+            <Icons />
+        </HashRouter>
     </Provider>,
 );
