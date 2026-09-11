3dbfff67d8bf358a6aa4608089b151cf376e4021
f.setrakov <f.setrakov@adguard.com>
2026-09-11 16:56:26 +0300

    all: disable legacy logger

--- internal/dhcpd/v6_unix.go
+++ internal/dhcpd/v6_unix.go
@@ -750,7 +750,7 @@ func (s *v6Server) Start(ctx context.Context) (err error) {
 		Time:          dhcpv6.GetTime(),
 	}
 
-	s.srv, err = server6.NewServer(iface.Name, nil, s.packetHandler, server6.WithDebugLogger())
+	s.srv, err = server6.NewServer(iface.Name, nil, s.packetHandler, s.opts()...)
 	if err != nil {
 		return err
 	}
@@ -766,6 +766,16 @@ func (s *v6Server) Start(ctx context.Context) (err error) {
 	}()
 
 	return nil
+}
+
+// opts returns the options for the DHCPv6 server.
+func (s *v6Server) opts() (opts []server6.ServerOpt) {
+	opts = []server6.ServerOpt{}
+	if log.GetLevel() != log.OFF {
+		opts = append(opts, server6.WithDebugLogger())
+	}
+
+	return opts
 }
 
 // Stop - stop server
